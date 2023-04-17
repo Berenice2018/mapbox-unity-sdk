1a35b26319a3127f6a20b0da68bbf4bc24a36ffa
Berenice2018 <terwey2012@gmail.com>
2023-04-17 21:56:22 +0200

    adds dummy data layer to DataExplorer demo scene

---
+++ Mapbox_ARFound/Assets/Scripts/AddWMTSTileset.cs
@@ -0,0 +1,13 @@
+using System.Diagnostics;
+using System.IO;
+using Mapbox.Map;
+using Mapbox.Unity.Map;
+using Mapbox.Unity.Utilities;
+using Mapbox.VectorTile;
+using UnityEngine;
+
+public class AddWMTSLayer : MonoBehaviour
+{
+  
+
+}
